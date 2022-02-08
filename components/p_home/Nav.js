95b0748e8a8536d7973cc4752173988382579cee
luomiao <1084459061@qq.com>
2022-02-09 02:56:44 +0800

    fix: amend nav

--- components/p_home/Nav.js
+++ components/p_home/Nav.js
@@ -3,32 +3,28 @@ import ReactSlick from 'react-slick'
 import Link from 'next/link'
 
 const Nav = ({ data = [] }) => {
-  const isNavrable = !!(data && data.length)
+  const isSwiperable = !!(data && data.length)
   const settings = {
-    arrows: true,
     dotsClass: 'nav-dots',
-    className: 'home-navs',
-    slidesToShow: 4,
-    slidesPerRow: 2,
     speed: 500,
-    // rows: 2,
-    infinite: isNavrable,
-    swipe: isNavrable,
-    lazyLoad: true,
-    dots: isNavrable,
-    // adaptiveHeight: isNavrable,
+    rows: 2,
+    dots: isSwiperable,
+    infinite: isSwiperable,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: isSwiperable,
   }
   return (
     <section className={s.nav}>
       <ReactSlick {...settings}>
         {data.map((item, index) => (
-          <div key={`${index}-${item.id}`} className={s.slidediv}>
-            <Link href="/course/detail/[id]" as={`course/detail/${item.id}`}>
-              <a className={s._19_60}>
-                <img src={item.img} className={s.navslide} />
-                <span className={s.title}>{item.title}</span>
-              </a>
-            </Link>
+          <div key={`${index}-${item.id}`}>
+            {/* 跳转连接 */}
+            {/* nav 图片 */}
+            <a className={s._19_60} href={`/course/detail/${item.id}`}>
+              <img src={item.img} className={s.slide} />
+              <span className={s.title}>{item.title}</span>
+            </a>
           </div>
         ))}
       </ReactSlick>
